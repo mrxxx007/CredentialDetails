e3c4e54b0c943071ab98d3b492d17f318d34bdc1
svpopov007 <svpopov007@gmail.com>
2017-04-20 23:06:28 +0300

    Initial commit

---
+++ src/main/java/FolderDifference/util/StringUtils.java
@@ -0,0 +1,10 @@
+package FolderDifference.util;
+
+/**
+ * Created by Admin on 25.09.2016.
+ */
+public final class StringUtils {
+    public static boolean isNullOrEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+}
